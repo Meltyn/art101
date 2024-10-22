6ae235fbec682584e6f0df15eb1c0489bef4ca72
Meltyn <94092337+Meltyn@users.noreply.github.com>
2024-10-21 23:31:29 -0700

    dog

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,15 +1,15 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Data Types and Variables
+// Author: Sarai Lopez
+// Date: 10/21/2024
 
-// Constants
+
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
-  // return results;
+  //return results;
 }
 
 function main() {
@@ -19,3 +19,22 @@ function main() {
 
 // let's get this party started
 main();
+
+//Define Variables
+make = "Ford";
+model = "Escorts";
+color = "Rusty";
+year = 1980;
+
+//calculate
+age = 2024 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
+
+// Constants
