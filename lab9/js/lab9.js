4e1d9e5a5f235cc45c5a3006a076a4f2461db89b
Meltyn <94092337+Meltyn@users.noreply.github.com>
2024-11-04 22:45:13 -0800

    lab9

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -6,24 +6,24 @@
 // add button to challenge section
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 // add a click listener to the challenge button
-$("button-challenge").click(function(){
+$("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
 });
 
 // add button to challenge section
-$("#problems").append("<button id='button-challenge'>Me when I</button>");
+$("#problems").append("<button id='button-problems'>Me when I</button>");
 // add a click listener to the challenge button
-$("button-challenge").click(function(){
+$("#button-problems").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
+    $("#problems").toggleClass("special");
 });
 
-$("#results").append("<button id='button-challenge'>Hunger</button>");
+$("#results").append("<button id='button-results'>Hunger</button>");
 // add a click listener to the challenge button
-$("button-challenge").click(function(){
+$("#button-results").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
+    $("#results").toggleClass("special");
 });
 
 
